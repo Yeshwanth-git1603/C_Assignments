5d8b84ccc8f43d9f5e07c40dfa3ee3dd698b042c
EC2 Default User <ec2-user@ip-172-31-18-79.ec2.internal>
2024-09-04 09:40:48 +0000

    c_programming

---
+++ inbuilt_functions/size_function/example1.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+
+int main(){
+    int x,y,z;
+    printf("enter the x value:");
+    scanf("%d",&x);
+    printf("enter the y value:");
+    scanf("%d",&y);
+    z=x+y;
+    printf("the value of z is: %d\n",z);
+    printf("the size of x is :%d\n",sizeof(x));
+    printf("the size of y is :%d\n",sizeof(y));
+    printf("the size of z is :%d",sizeof(z));
+ 
+}
